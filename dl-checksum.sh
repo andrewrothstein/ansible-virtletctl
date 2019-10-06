37ce65f203495125c44abb5b1da898e5a45c6019
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-06 14:13:17 -0500

    delint

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env sh
-VER=v1.5.1
+VER=${1:-v1.5.1}
 DIR=~/Downloads
 MIRROR=https://github.com/Mirantis/virtlet/releases/download/${VER}
 
 dl()
 {
-    PLATFORM=$1
-    SUFFIX=${2:-}
-    FILE=virtletctl${SUFFIX}
-    URL=${MIRROR}/${FILE}
-    wget -O $DIR/${FILE}-${VER} $URL
+    local os=$1
+    local suffix=${2:-}
+    local file=virtletctl-${os}-${VER}
+    local lfile=$DIR/$file
+    local url=${MIRROR}/virtletctl${suffix}
+
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $os $(sha256sum $lfile | awk '{print $1}')
 }
 
+printf "  %s:\n" $VER
 dl linux
 dl darwin .darwin
-sha256sum $DIR/virtletctl*-$VER
+
 
